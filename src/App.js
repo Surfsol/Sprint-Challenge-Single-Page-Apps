0d936cb54552d82fa7d93c629b62796b9cf6e82a
Surfsol <surfsol@protonamil.com>
2019-09-20 15:00:18 -0300

    added search

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React from "react";
-import TabNav from "./components/TabNav.js";
-import Header from "./components/Header.js";
+import React, { useState } from 'react';
+import {Route} from "react-router-dom"; 
+//import TabNav from "./components/TabNav.js";
+//import Header from "./components/Header.js";
 //import AppRouter from "./components/AppRouter.js";
 //<Route exact path="/characters" component= {CharacterList} />
  //     <Route exact path="/location" component= {LocationList} />/>
  //     <Route exact path="/episodes" component= {EpisodesList} />/>
-//import CharacterList from './components/CharacterList'
-//import LocationList from './components/LocationsList'
-import EpisodesList from './components/EpisodesList'
+import CharacterList from './components/CharacterList'
+import SearchForm from './components/SearchForm'
+//import EpisodesList from './components/EpisodesList'
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <main>
+//       <Header />
+//       <TabNav />
+//       <CharacterList />
+//     </main>
+//   );
+// }
+
+ const App = () => {
+  const [savedperson, setSavedPerson] = useState( [] );
+
+  const addToSavedList = person => {
+    setSavedList( [...savedList, person] );
+  };
+
   return (
-    <main>
-      <Header />
-      <TabNav />
-      <EpisodesList />
-    </main>
+    <div>
+      <Route exact path="/" component={CharacterList}/>
+      <Route path="/searchform/:id" render={props => <SearchForm {...props} />}/>
+    </div>
   );
-}
+};
+
+export default App;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,7 +1,31 @@
 import React, { useState } from "react";
+import axios from 'axios';
 
-export default function SearchForm({ onSearch }) {
+const Person = (props) => {
+
+const [person, setPerson] = useState({});
+  console.log(`search`,props)
+
+  const personId = props.match.params.id
+
+  useEffect(() => {
+    const id = personId ;
+    console.log(id)
+       axios
+        .get(`https://rickandmortyapi.com/api/${id}/`)
+        .then(response => {
+          console.log(response.data)
+          setMovie(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+  
+  },[props.id]);
+
+ function SearchForm({ onSearch }) {
   // TODO: Add stateful logic for query/form data
+  const
   return (
     <section className="search-form">
       <form onSubmit={() => onSearch(name)}>
@@ -16,3 +40,4 @@ export default function SearchForm({ onSearch }) {
     </section>
   );
 }
+export default SearchForm
